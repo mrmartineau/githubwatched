bc14c41d499ca574450a960f279c52c90d0bbbd2
Zander Martineau <zander@martineau.tv>
2011-12-18 01:06:14 +0000

    Add gists

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 var defaultUser = 'mrmartineau';
-var i = 100;
 
 //show loading graphic
 function loading() {
@@ -12,7 +11,6 @@ function loaded() {
 }
 
 function loadUserAndRepo(username, type, id) {
-	i++;
 	loading();
 	//
 	// repos
@@ -32,7 +30,34 @@ function loadUserAndRepo(username, type, id) {
 				'</li>');
 			});
 			$('<ol/>', {
-				'class': '',
+				'class': 'ui_list',
+				html: items.join('')
+			}).appendTo(id).hide().fadeIn();
+			loaded();
+		}
+	});
+}
+
+function loadGists(username, type, id) {
+	loading();
+	//
+	// repos
+	//
+	$.ajax({
+		url: "https://api.github.com/"+ type +"?page=1&per_page=100&callback=?",
+		context: id,
+		dataType: "jsonp",
+		success: function(data){
+			var items = [];
+			$.each(data.data, function(i,item){
+				items.push('<li>'+
+				'<a href="' + item.html_url +'" title="gist: ' + item.id +'" class="item_name">gist: ' + item.id +'</a>'+
+				'<a href="' + item.user.url +'" title="' + item.user.login +'" class="item_owner">' + item.user.login +'</a>'+
+				'<div class="item_description">' + item.description +'</div>'+
+				'</li>');
+			});
+			$('<ol/>', {
+				'class': 'ui_list',
 				html: items.join('')
 			}).appendTo(id).hide().fadeIn();
 			loaded();
@@ -44,8 +69,11 @@ function loadUserAndRepo(username, type, id) {
 
 	//same as $(document).ready();
 	$(function() {
-			loadUserAndRepo(defaultUser, 'watched', '#watched');
-			loadUserAndRepo(defaultUser, 'repos', '#your');
+		// Load default user
+		loadUserAndRepo(defaultUser, 'watched', '#watched');
+		loadUserAndRepo(defaultUser, 'repos', '#your');
+		loadGists(defaultUser, 'gists/starred', '#starred_gists');
+		loadGists(defaultUser, 'users/mrmartineau/gists', '#your_gists');
 
 		//query string
 		var qsArray = window.location.href.split('?');
@@ -57,6 +85,8 @@ function loadUserAndRepo(username, type, id) {
 			$('#username_entry').val(qs);
 			loadUserAndRepo(qs, 'watched', '#watched');
 			loadUserAndRepo(qs, 'repos', '#your');
+			loadGists(qs, 'gists/starred', '#starred_gists');
+			loadGists(qs, 'gists/public', '#your_gists');
 		}
 		//when the url textbox is used
 		$('form').submit(function(){
@@ -64,8 +94,47 @@ function loadUserAndRepo(username, type, id) {
 			loading();
 			loadUserAndRepo($('#username_entry').val(), 'watched', '#watched');
 			loadUserAndRepo($('#username_entry').val(), 'repos', '#your');
+			loadGists($('#username_entry').val(), 'gists/starred', '#starred_gists');
+			loadGists($('#username_entry').val(), 'gists/public', '#your_gists');
 			return false;
 		});
+
+		// TABS from Skeleton (with my own ARIA sauce added)
+		var tabs = $('ul.tabs');
+
+		tabs.each(function (i) {
+			//Get all tabs
+			var tab = $(this).find('> li > a');
+			tab.each(function (i) {
+				var tabid = $(this).attr('href'),
+					newTabid = tabid.slice(1);
+				if (tabid.charAt(0) === '#') {
+					$('#' + newTabid).each(function (i) {
+						$(this).attr('aria-labelledby', newTabid + '_tab');
+					});
+
+					$(this).attr({
+						role: 'tab',
+						id: newTabid + '_tab'
+					}).attr('aria-describedby', newTabid);
+				}
+
+			}).click(function (e) {
+				var contentLocation = $(this).attr('href');
+				if (contentLocation.charAt(0) === "#") {
+					e.preventDefault();
+					tab.removeClass('active');
+					$(this).addClass('active');
+					$(contentLocation).show()
+						.attr('role', 'aria-selected')
+						.addClass('active')
+						.siblings()
+						.attr('role', 'aria-hidden')
+						.hide()
+						.removeClass('active');
+				}
+			});
+		}); //END TABS
 
 
 
